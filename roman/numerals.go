13d399571e7bf5f85cd9fe0bd24262b6cc81afd8
g-dx <g-dx@users.noreply.github.com>
2017-04-24 17:21:21 +0100

    Added basic implementation for Decode.

--- roman/numerals.go
+++ roman/numerals.go
@@ -18,24 +18,24 @@ func Encode(n uint) string {
 	// Hundreds
 	if (n > 100 && n <= 1000) {
 		hundreds := n / 100
-		numerals += base(hundreds, "C", "D", "M")
+		numerals += toNumerals(hundreds, "C", "D", "M")
 		n -= hundreds * 100
 	}
 
 	// Tens
 	if (n > 10 && n <= 100) {
 		tens := n / 10
-		numerals += base(tens, "X", "L", "C")
+		numerals += toNumerals(tens, "X", "L", "C")
 		n -= tens * 10
 	}
 
 	// Ones
-	numerals += base(n, "I", "V", "X")
+	numerals += toNumerals(n, "I", "V", "X")
 	return numerals
 }
 
-func base(n uint, one string, five string, ten string) string {
-	switch(n) {
+func toNumerals(n uint, one string, five string, ten string) string {
+	switch n {
 	case 0 : return ""
 	case 1 : return one
 	case 2 : return one + one
@@ -48,10 +48,47 @@ func base(n uint, one string, five string, ten string) string {
 	case 9 : return one + ten
 	case 10: return ten
 	default:
-		panic(fmt.Sprintf("Expected: value between 1 - 10, Actual: %s", n))
+		panic(fmt.Sprintf("Unexpected value: %s", n))
 	}
 }
 
-func decode(n string) uint {
-	return 0
+func Decode(s string) int {
+
+	total, start, pos := 0, 0, 0
+
+	// Thousands
+	for ; pos < len(s) && s[pos] == 'M'; pos++ {}
+	total += (pos - start) * 1000
+
+	// Hundreds
+	for start = pos; pos < len(s) && (s[pos] == 'C' ||  s[pos] == 'D' || s[pos] == 'M'); pos++ {}
+	total += fromNumerals(s[start:pos], "C", "D", "M") * 100
+
+	// Tens
+	for start = pos; pos < len(s) && (s[pos] == 'X' ||  s[pos] == 'L' || s[pos] == 'C'); pos++ {}
+	total += fromNumerals(s[start:pos], "X", "L", "C") * 10
+
+	// Ones
+	if (pos < len(s)) {
+		total += fromNumerals(s[pos:], "I", "V", "X")
+	}
+	return total
+}
+
+func fromNumerals(s string, one string, five string, ten string) int {
+	switch s {
+	case "" : return 0
+	case one: return 1
+	case one + one: return 2
+	case one + one + one: return 3
+	case one + five: return 4
+	case five: return 5
+	case five + one: return 6
+	case five + one + one: return 7
+	case five + one + one + one: return 8
+	case one + ten: return 9
+	case ten: return 10
+	default:
+		panic(fmt.Sprintf("Unexpected value: %s", s))
+	}
 }
--- roman/numerals_test.go
+++ roman/numerals_test.go
@@ -1,39 +1,52 @@
 package roman
 
-import "testing"
+import (
+	"testing"
+)
+
+var data = []struct {
+	val      uint
+	numerals string
+}{
+	{1, "I"},
+	{2, "II"},
+	{3, "III"},
+	{4, "IV"},
+	{5, "V"},
+	{6, "VI"},
+	{7, "VII"},
+	{8, "VIII"},
+	{9, "IX"},
+	{10, "X"},
+	{11, "XI"},
+	{49, "XLIX"},
+	{99, "XCIX"},
+	{100, "C"},
+	{101, "CI"},
+	{1929, "MCMXXIX"},
+	{1998, "MCMXCVIII"},
+	{1999, "MCMXCIX"},
+	{2000, "MM"},
+	{2001, "MMI"},
+	{1333, "MCCCXXXIII"},
+}
 
 func TestEncode(t *testing.T) {
 
-	var data = []struct {
-		in  uint
-		out string
-	}{
-		{1, "I"},
-		{2, "II"},
-		{3, "III"},
-		{4, "IV"},
-		{5, "V"},
-		{6, "VI"},
-		{7, "VII"},
-		{8, "VIII"},
-		{9, "IX"},
-		{10, "X"},
-		{11, "XI"},
-		{49, "XLIX"},
-		{99, "XCIX"},
-		{100, "C"},
-		{101, "CI"},
-		{1998, "MCMXCVIII"},
-		{1999, "MCMXCIX"},
-		{2000, "MM"},
-		{2001, "MMI"},
-		{1333, "MCCCXXXIII"},
+	for _, tt := range data {
+		v := Encode(tt.val)
+		if v != tt.numerals {
+			t.Errorf("Encode(%d) => got %s, want %s", tt.val, v, tt.numerals)
+		}
 	}
+}
+
+func TestDecode(t *testing.T) {
 
 	for _, tt := range data {
-		v := Encode(tt.in)
-		if v != tt.out {
-			t.Errorf("Encode(%d) => got %s, want %s", tt.in, v, tt.out)
+		v := Decode(tt.numerals)
+		if v != int(tt.val) {
+			t.Errorf("Decode(%s) => got %d, want %d", tt.numerals, v, tt.val)
 		}
 	}
 }
